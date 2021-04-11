0b7064bbe4fb1adb90a2fe08c2685dbaf5e99148
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-04-11 12:02:42 +0530

    quick sort from scratch

---
+++ Searching & Sorting/Sorting scratch/05_quick_sort.py
@@ -0,0 +1,31 @@
+
+
+def partition(arr, low, high):
+
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i+1], arr[high] = arr[high], arr[i+1]
+
+    return i+1
+
+
+def quickSort(arr, low, high):
+
+    if low < high:
+        mid = partition(arr, low, high)
+
+        quickSort(arr, low, mid-1)
+        quickSort(arr, mid+1, high)
+
+
+arr = list(map(int, input().split()))
+
+quickSort(arr, 0, len(arr) - 1)
+print(arr)
