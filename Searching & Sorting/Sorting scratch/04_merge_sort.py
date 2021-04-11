14b0f196c96c6f3a6d43eba7df0f3b5b89f426f8
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-04-11 08:39:44 +0530

    merge sort from scratch

---
+++ Searching & Sorting/Sorting scratch/04_merge_sort.py
@@ -0,0 +1,60 @@
+
+def merge(arr, low, mid, high):
+
+    n1 = mid - low + 1
+    n2 = high - mid
+
+    left_arr = []
+    right_arr = []
+
+    for i in range(0, n1):
+        left_arr.append(arr[low + i])
+
+    for j in range(0, n2):
+        right_arr.append(arr[mid + 1 + j])
+
+    i = j = 0
+    k = low
+
+    while i < n1 and j < n2:
+        if left_arr[i] <= right_arr[j]:
+            arr[k] = left_arr[i]
+            i += 1
+
+        else:
+            arr[k] = right_arr[j]
+            j += 1
+
+        k += 1
+
+    while i < n1:
+        arr[k] = left_arr[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        arr[k] = right_arr[j]
+        j += 1
+        k += 1
+
+
+def divide(arr, low, high):
+
+    if(low < high):
+
+        mid = low + ((high - low)//2)  # Never miss floor
+
+        divide(arr, low, mid)
+        divide(arr, mid+1, high)
+
+        merge(arr, low, mid, high)
+
+
+arr = list(map(int, input().split()))
+
+
+high = len(arr) - 1
+
+divide(arr, 0, high)
+
+print(arr)
