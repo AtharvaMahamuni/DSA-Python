0d7e2a79eda945e605fcf6017df384d609e91700
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-04-12 14:40:55 +0530

    string questions

---
+++ String/01_reverse.py
@@ -0,0 +1,14 @@
+
+
+s = [x for x in "hello"]
+
+print(s)
+
+# s = [x for x in reversed(s)]
+
+# s = s[::-1]
+
+s.reverse()
+
+print(s)
+
---
+++ String/02_palindrome.py
@@ -0,0 +1,10 @@
+
+
+def isPalindrome(S):
+    if S == S[::-1]:
+        return 1
+    else:
+        return 0
+
+
+print(isPalindrome("abba"))
