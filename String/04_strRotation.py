833180b573a10566deafd8fecff7f853ffa0a1f1
Atharva Mahamuni <57866448+AtharvaMahamuni@users.noreply.github.com>
2021-04-29 11:28:15 +0530

    where the 2 strings are rotatin of each other

---
+++ String/04_strRotation.py
@@ -0,0 +1,17 @@
+
+def areRotation(str1, str2):
+
+    str1 += str1
+
+    for i in range(len(str2)):
+
+        if str2 == str1[i:len(str2)+i]:
+            return True
+
+    return False
+
+
+
+print(areRotation("ABACD", "CDABA"))
+print(areRotation("AACD", "ACDA"))
+print(areRotation("AACA", "ACDA"))
