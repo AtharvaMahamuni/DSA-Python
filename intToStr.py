a9b946dbb1a3b3ecb5fa3e0cbff15e13add81878
AtharvaMahamuni <atharvamahamuni54@gmail.com>
2022-04-14 22:38:04 +0530

    INT TO STRING CODES

---
+++ intToStr.py
@@ -0,0 +1,56 @@
+# # code 1 ---
+# number = 999
+ 
+# # Type of number variable
+# print(type(number))
+ 
+# # convertint the number into string
+# number_string = str(number)
+ 
+# # type of converted number
+# print(type(number_string))
+
+
+
+# code 2 ---
+
+# # Initializing the number variable
+# number = 10
+ 
+# # Type of number variable
+# print(type(number))
+ 
+# # Type of converted number
+# number_string = "% s" % number
+# print(type(number_string))
+
+
+# code 3 ---
+
+# # Initializing the number variable
+# number = 10
+ 
+# # Type of number variable
+# print(type(number))
+ 
+# # Converting number into string
+# number_string = "{}".format(number)
+
+# # Type of converted number_string
+# print(type(number_string))
+
+
+
+# code 4 ---
+
+# Initializing the number varaible
+number = 10
+ 
+# Type of number variable
+print(type(number))
+ 
+# Converting number into string
+number_string = f'{number}'
+ 
+# Type of converted number_string
+print(type(number_string))
